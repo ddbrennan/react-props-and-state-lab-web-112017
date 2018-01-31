dd9219fefe496df95bfbf34e370187f9a51e1b65
ddbrennan <ddbren@gmail.com>
2018-01-31 17:56:30 -0500

    pet shop working

--- src/components/App.js
+++ src/components/App.js
@@ -25,15 +25,40 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters filters={ this.state.filters }
+                onChangeType={ this.onChangeType }
+                onFindPetsClick={ this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={ this.state.pets } onAdoptPet={ this.onAdoptPet } adoptedPets={ this.state.adoptedPets }/>
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  onChangeType = (type) => {
+    console.log(type, "Type")
+    this.setState({
+      filters: { type }
+    })
+  }
+
+  onFindPetsClick = () => {
+    if (this.state.filters.type === "all") {
+      fetch("/api/pets").then(res => res.json()).then(pets => this.setState({ pets }))
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`).then(res => res.json()).then(pets => this.setState({ pets }))
+    }
+  }
+
+  onAdoptPet = (pet) => {
+    let arr = [...this.state.adoptedPets, pet]
+    this.setState({
+      adoptedPets: arr
+    })
+
   }
 }
 
